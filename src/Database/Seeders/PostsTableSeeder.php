9d98848202c8b1cc81cadc88be1e12944d5cddbc
Tuhin <digitaldreams40@gmail.com>
2017-12-29 16:05:50 +0000

    Editing PostsTableSeeder

--- src/Database/Seeders/PostsTableSeeder.php
+++ src/Database/Seeders/PostsTableSeeder.php
@@ -22,15 +22,21 @@ class PostsTableSeeder extends Seeder
         $user = config('auth.providers.users.model');
         $user = new $user;
         $user = $user->first();
-        Post::create([
-            'user_id' => $user->id,
-            'title' => $generator->realText(120),
-            'slug' => $generator->slug,
-            'body' => $generator->realText(),
-            'status' => 'published',
-            'category_id' => Category::all()->random(1)->first()->id,
-            'image' => $generator->image(storage_path('app/public')),
-        ]);
+
+        for ($i = 0; $i < 10; $i++) {
+            $image = $generator->image(storage_path('app/public/images'));
+            $fileName = pathinfo($image, PATHINFO_BASENAME);
+            Post::create([
+                'user_id' => $user->id,
+                'title' => $generator->realText(40),
+                'slug' => $generator->slug,
+                'body' => $generator->realText(),
+                'status' => 'published',
+                'category_id' => Category::all()->random(1)->first()->id,
+                'image' => 'images/' . $fileName,
+            ]);
+        }
+
     }
 
 }
