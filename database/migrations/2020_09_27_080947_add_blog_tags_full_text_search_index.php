a331fa105aaaaa61ddae9a8129cbdc4b24e6e42b
Tuhin Bepari <digitaldreams40@gmail.com>
2020-10-06 21:05:03 +0600

    Blog Tag Full Text added

---
+++ database/migrations/2020_09_27_080947_add_blog_tags_full_text_search_index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddBlogTagsFullTextSearchIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog_tags', function (Blueprint $table) {
+            DB::statement('ALTER TABLE blog_tags ADD FULLTEXT tags_full_text_index(name)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog_tags', function (Blueprint $table) {
+            $table->dropIndex('tags_full_text_index');
+        });
+    }
+}
