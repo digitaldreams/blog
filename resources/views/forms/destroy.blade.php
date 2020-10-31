1f20f5e6240d1bef0fee7a221c9f6683ee7bc28d
Tuhin Bepari <digitaldreams40@gmail.com>
2020-10-31 09:03:51 +0600

    WOrking with Frontend

--- resources/views/forms/destroy.blade.php
+++ resources/views/forms/destroy.blade.php
@@ -4,5 +4,6 @@
     {{csrf_field()}}
     {{method_field('DELETE')}}
     <button type="submit" class="btn btn-light cursor-pointer  btn-sm">
-        <i class="text-danger fa fa-times"></i></button>
+        Delete
+    </button>
 </form>
--- resources/views/includes/post_dropdown_menu.blade.php
+++ resources/views/includes/post_dropdown_menu.blade.php
@@ -1,6 +1,8 @@
 <div class="dropdown dropleft" id="dropdown-{{$record->id}}">
-    <a href="#" class="fa fa-ellipsis-v" data-toggle="dropdown" role="button"
-       aria-expanded="false">
+    <a href="#" data-toggle="dropdown" role="button"
+       aria-expanded="false"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+        </svg>
     </a>
     <ul class="dropdown-menu list-group-flush">
         <li class="list-group-item">
@@ -11,7 +13,6 @@
         </li>
         <li class="list-group-item">
             @include('blog::forms.destroy',['route'=>route('blog::posts.destroy',$record->slug)])
-            Destroy
         </li>
         @if(is_object($record->image))
             @can('update',$record->image)
